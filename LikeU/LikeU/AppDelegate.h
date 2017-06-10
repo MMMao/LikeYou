2e29b86e3b87a312a8ad9c74eed4bbf92b94994b
Eric <578036608@qq.com>
2017-06-10 12:07:01 +0800

    创建工程

---
+++ LikeU/LikeU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LikeU
+//
+//  Created by Ericmao on 2017/6/10.
+//  Copyright © 2017年 zhengya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
